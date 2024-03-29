eba257211d48084667aea611ab310df128f9502b
manishreddyyeruva <yeruvamanish22@gmail.com>
2024-03-29 16:09:08 -0400

    configured mutliple users

--- src/main/java/com/springSecurity/springSecurityImplementation/config/ApplicationSecurityConfig.java
+++ src/main/java/com/springSecurity/springSecurityImplementation/config/ApplicationSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,29 +21,25 @@ public class ApplicationSecurityConfig {
 
         http.authorizeHttpRequests((requests)
                 -> requests.requestMatchers(
-                        "/account/myAccount","/balance/myBalance",
-                "/cardDetails","/loans/loanDetails").authenticated()
+                        "/myAccount","/myBalance",
+                "/cardDetails","/loanDetails").authenticated()
                 .requestMatchers("/notices","/contacts").permitAll())
                  .formLogin(withDefaults())
                 .httpBasic(withDefaults());
 
         return http.build();
 
-
     }
-
-    //deny all
-        //            http.authorizeHttpRequests((requests)
-        //            -> requests.anyRequest().denyAll())
-        //            .formLogin(withDefaults())
-        //            .httpBasic(withDefaults());
-        //        return http.build();
-
-
-    //permit all
-    //            http.authorizeHttpRequests((requests)
-    //            -> requests.anyRequest().permitAll())
-    //            .formLogin(withDefaults())
-    //            .httpBasic(withDefaults());
-    //        return http.build();
+        @Bean
+        public InMemoryUserDetailsManager userDetailsService(){
+            UserDetails admin= User.withDefaultPasswordEncoder()
+                    .username("admin")
+                    .password("12345")
+                    .authorities("admin").build();
+            UserDetails user= User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("12345")
+                    .authorities("read").build();
+            return new InMemoryUserDetailsManager(admin,user);
+        }
 }
--- src/main/java/com/springSecurity/springSecurityImplementation/controller/BalanceController.java
+++ src/main/java/com/springSecurity/springSecurityImplementation/controller/BalanceController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/balance")
+@RestController
 public class BalanceController {
     @GetMapping("/mybalance")
     public String getBalance(){
